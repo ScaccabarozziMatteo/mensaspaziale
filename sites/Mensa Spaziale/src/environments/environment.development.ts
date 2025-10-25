a5c9fb6a50b1aebafd6bd83dfd889e9c1210322d
Matteo <matteo3.scaccabarozzi@mail.polimi.it>
2025-10-25 15:41:30 +0200

    Clean folder

--- sites/Mensa Spaziale/src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment: {
-  appwriteEndpoint: string;
-  appwriteProjectId: string;
-  appwriteProjectName: string;
-} = {
-  appwriteEndpoint: '[appwriteEndpoint]',
-  appwriteProjectId: '[appwriteProjectId]',
-  appwriteProjectName: '[appwriteProjectName]',
-};
