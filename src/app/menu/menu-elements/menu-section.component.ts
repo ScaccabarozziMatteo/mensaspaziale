3437947cad9bd149250b373a1ff3f287535351a3
Matteo <matteo3.scaccabarozzi@mail.polimi.it>
2025-11-05 23:07:08 +0100

    Massive UI update, add github link

---
+++ src/app/menu/menu-elements/menu-section.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'menu-section',
+  templateUrl: './menu-section.component.html',
+  styleUrls: ['../menu.component.css'] 
+})
+export class MenuSectionComponent implements OnInit {
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
